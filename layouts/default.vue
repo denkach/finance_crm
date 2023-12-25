da9959fb61e4e84319576d231833b37d1a808dca
denkach <denkach2211@gmail.com>
2023-12-25 17:02:34 +0100

    added form for authorization

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,6 @@
     <div>
       <slot />
     </div>
-    <UiButton/>
   </section>
 </template>
 
---
+++ pages/login.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+useHead({
+  title: "Login | CRM System",
+});
+
+const name = ref("");
+const email = ref("");
+const password = ref("");
+
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5">
+      <h1 class="text-2xl font-bold text-center mb-5 ">Login</h1>
+
+      <form>
+        <UiInput placeholder="Name" type="name" class="mb-3"
+          v-model="name"
+        />
+        <UiInput placeholder="Email" type="email" class="mb-3"
+            v-model="email"
+          />
+        <UiInput placeholder="Password" type="password" class="mb-3"
+            v-model="password"
+        />
+
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button">Login</UiButton>
+          <UiButton type="button">Register</UiButton>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>

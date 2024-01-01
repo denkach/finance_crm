7c23a68f5e9f616af88b3be7b039da163ee62ff7
denkach <denkach2211@gmail.com>
2024-01-01 21:48:01 +0100

    logout

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -1,5 +1,17 @@
 <script lang="ts" setup>
+import { account } from '@/lib/appwrite';
 
+const store = useAuthStore();
+const isLoadingStore = useIsLoadingStore();
+const router = useRouter();
+
+const logout = async () => {
+  isLoadingStore.set(true);
+  await account.deleteSession('current');
+  store.clear();
+  await router.push('/login');
+  isLoadingStore.set(false);
+}
 </script>
 
 <template>
@@ -7,7 +19,7 @@
     <NuxtLink to="/" class="mb-10 block">
       <NuxtImg src="/logo.svg" alt="" width="120px" class="mx-auto"/>
     </NuxtLink>
-    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+    <button @click="logout" class="absolute top-2 right-3 transition-colors hover:text-primary">
       <Icon name="line-md:logout" size="20"/>
     </button>
     <LayoutMenu />

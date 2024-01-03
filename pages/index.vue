3712009e1714bb1116e8cc4b2b69e9c493157c29
denkach <denkach2211@gmail.com>
2024-01-03 23:47:09 +0100

    some style changes

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,12 @@
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System by Denkach</h1>
+    <div>
+      <UiCard class="mb-3" draggable="true">
+        <UiCardHeader role="button">name card</UiCardHeader>
+        <UiCardContent>Company</UiCardContent>
+        <UiCardFooter>Date</UiCardFooter>
+      </UiCard>
+    </div>
   </div>
 </template>

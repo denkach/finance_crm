10bbd773d30d20b6ba98ca80f6443b99ff8aa78f
denkach <denkach2211@gmail.com>
2024-01-10 10:44:36 +0100

    cards

--- pages/index.vue
+++ pages/index.vue
@@ -8,19 +8,27 @@ useHead({
 
 const dragCard = ref<ICard | null>(null);
 const sourceColumn = ref<IColumn | null>(null);
-useKanbanQuery();
+const { data,  isLoading, refetch } = useKanbanQuery();
 
 </script>
 
 <template>
   <div class="p-10">
     <h1 class="font-bold text-2xl mb-10">CRM System by Denkach</h1>
-    <div>
-      <UiCard class="mb-3" draggable="true">
-        <UiCardHeader role="button">name card</UiCardHeader>
-        <UiCardContent>Company</UiCardContent>
-        <UiCardFooter>Date</UiCardFooter>
-      </UiCard>
+    <div v-if="isLoading">Loading...</div>
+    <div class="grid grid-cols-5 gap-16">
+      <div v-for="(column, index) in data" :key="column.id">
+        <div class="rounded bg-slate-700 py-1 px-5 mb-2 text-center">
+          {{ column.name }}
+        </div>
+        <div>
+          <UiCard class="mb-3" draggable="true">
+            <UiCardHeader role="button">name card</UiCardHeader>
+            <UiCardContent>Company</UiCardContent>
+            <UiCardFooter>Date</UiCardFooter>
+          </UiCard>
+        </div>
+      </div>
     </div>
   </div>
-</template>
+</template>newBoard;
